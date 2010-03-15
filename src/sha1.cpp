03bdaccc19e87625dfdb1e15c673b3b389a2731f
quicky@quickux <quicky@quickux>
2010-03-15 22:09:10 +0100

    Initial import

---
+++ src/sha1.cpp
@@ -0,0 +1,12 @@
+
+#include "sha1.h"
+
+const uint32_t sha1::m_constants[4] =
+  { 
+    0x5a827999, // 0 to 19
+    0x6ed9eba1, //20 to 39
+    0x8f1bbcdc, //40 to 59
+    0xca62c1d6  //60 to 79
+  };
+
+//EOF
