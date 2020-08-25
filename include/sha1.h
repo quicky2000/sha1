7c18e6bf3cf2505afe49c7b79808dd64a816ccce
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2020-08-25 08:38:36 +0200

    Make algorithm less verbose

--- include/sha1.h
+++ include/sha1.h
@@ -105,15 +105,21 @@ sha1::sha1( const uint8_t * p_data
                    }
 
 {
+#ifdef VERBOSE_SHA1
     std::cout << "Size in bit = " << p_size_bit << " => 0x" << std::hex << p_size_bit << std::dec << std::endl ;
+#endif // VERBOSE_SHA1
 
     // Computing length complement
     uint32_t l_complement_size = (448 - (p_size_bit + 1 )) % 512;
+#ifdef VERBOSE_SHA1
     std::cout << "Number of padding zeros = " << l_complement_size << std::endl ;
-  
+#endif // VERBOSE_SHA1
+
     uint32_t l_nb_blocks = ( p_size_bit + 1 + l_complement_size + 64) / 512;
-  
+
+#ifdef VERBOSE_SHA1
     std::cout << "Nb blocks = " << l_nb_blocks << std::endl ;
+#endif // VERBOSE_SHA1
 
     // Loop on data blocks
     for(uint64_t l_block_index = 0; l_block_index < l_nb_blocks ; ++l_block_index)
@@ -123,13 +129,19 @@ sha1::sha1( const uint8_t * p_data
 
         // Init block with datas
         //--------------------------
+#ifdef VERBOSE_SHA1
         std::cout <<  "Prepare block[" << l_block_index << "]" << std::endl ;
+#endif // VERBOSE_SHA1
 
         // Check if we are in a complete block
+#ifdef VERBOSE_SHA1
         std::cout << "check if this is a complete block" << std::endl;
+#endif // VERBOSE_SHA1
         if((l_block_index + 1) * 512 <= p_size_bit)
         {
+#ifdef VERBOSE_SHA1
             std::cout <<  "Copy complete block" << std::endl ;
+#endif // VERBOSE_SHA1
             for(uint32_t l_byte_index = 0; l_byte_index < 64; ++l_byte_index)
             {
                 l_working_block[l_byte_index / 4] |= ((uint32_t)p_data[l_byte_index]) << (24 - 8 * (l_byte_index % 4 ));
@@ -138,33 +150,47 @@ sha1::sha1( const uint8_t * p_data
         else
         {
             uint32_t l_rest_size_bits = (p_size_bit % 512 );
-            uint32_t l_rest_size_word = 1 + l_rest_size_bits / 32;
+#ifdef VERBOSE_SHA1
             std::cout << "size of incomplete block in bits = " << l_rest_size_bits << std::endl ;
+            uint32_t l_rest_size_word = 1 + l_rest_size_bits / 32;
             std::cout << "size of incomplete block in word = " << l_rest_size_word << std::endl ;
+#endif // VERBOSE_SHA1
             // Check if there are remaining datas to copy
+#ifdef VERBOSE_SHA1
             std::cout << "Check if there are less than 512 data bits to copy" << std::endl ;
+#endif // VERBOSE_SHA1
             if((l_block_index * 512 + l_rest_size_bits) == p_size_bit)
             {
+#ifdef VERBOSE_SHA1
                 std::cout <<  "Copy partial block" << std::endl ;
+#endif // VERBOSE_SHA1
                 //	  display_block(l_working_block);
                 for(uint32_t l_byte_index = 0; l_byte_index < l_rest_size_bits / 8; ++l_byte_index)
                 {
                     l_working_block[l_byte_index / 4] |= ((uint32_t)p_data[l_byte_index]) << (24 - 8 * (l_byte_index % 4 ));
                 }
+#ifdef VERBOSE_SHA1
                 display_block(l_working_block);
+#endif // VERBOSE_SHA1
             }
 
             uint32_t l_reset_word_start_index = 0;
             uint32_t l_reset_word_end_index = 16;
             // Check if we are in the block where to put the additional 1 bit
+#ifdef VERBOSE_SHA1
             std::cout << "Check if we are in the block where to put the additional 1 bit" << std::endl ;
+#endif // VERBOSE_SHA1
             if((p_size_bit  / 512) == l_block_index)
             {
+#ifdef VERBOSE_SHA1
                 std::cout << "Setting additional bit to 1" << std::endl ;
+#endif // VERBOSE_SHA1
                 // Setting one additional bit to 1
+#ifdef VERBOSE_SHA1
                 std::cout << "Index of block where to set the additional bit to 1 = " << (l_rest_size_bits / 32) << std::endl ;
                 std::cout << "Shifting of " << (31 - l_rest_size_bits) << std::endl ;
                 std::cout << "Mask = 0x" << std::hex <<  ( 1u << (31 - l_rest_size_bits)) << std::dec<< std::endl ;
+#endif // VERBOSE_SHA1
                 l_working_block[(l_rest_size_bits) / 32] |=  1u << (31 - l_rest_size_bits);
 	      
                 //	  display_block(l_working_block);
@@ -173,10 +199,10 @@ sha1::sha1( const uint8_t * p_data
             }
 
             // Adding the size
-            std::cout << "Check if we are in the latest block" << std::endl;
+            // "Check if we are in the latest block
             if(l_block_index + 1 == l_nb_blocks)
             {
-                std::cout << "Adding the size" << std::endl ;
+                // Adding the size on 64 bits
                 l_working_block[14] = p_size_bit >> 32u;
                 l_working_block[15] = (uint32_t)(p_size_bit & 0xFFFFFFFF);
                 l_reset_word_end_index = 14;
@@ -185,16 +211,20 @@ sha1::sha1( const uint8_t * p_data
             // Setting rest of word to 0
             for(uint32_t l_word_index = l_reset_word_start_index ; l_word_index < l_reset_word_end_index; ++l_word_index)
             {
+#ifdef VERBOSE_SHA1
                 std::cout << "Setting word[" << l_word_index << "] to 0" << std::endl ;
+#endif // VERBOSE_SHA1
                 l_working_block[l_word_index] = 0;
             }
 
             //	  display_block(l_working_block);
         }
 
+#ifdef VERBOSE_SHA1
         // Display block to treat
         //---------------------------
         display_block(l_working_block);
+#endif // VERBOSE_SHA1
 
         // Initialising word array
         //----------------------------
@@ -204,15 +234,19 @@ sha1::sha1( const uint8_t * p_data
         // Computing the other words
         for(uint32_t l_word_index = 16;l_word_index < 80; ++l_word_index)
         {
+#ifdef VERBOSE_SHA1
             std::cout << "Computing Word[" << l_word_index << "]" << std::endl ;
+#endif // VERBOSE_SHA1
             m_words[l_word_index] = rotl(m_words[l_word_index - 3] ^ m_words[l_word_index - 8] ^ m_words[l_word_index - 14] ^ m_words[l_word_index - 16],1);
         }
 
+#ifdef VERBOSE_SHA1
         //display words
         for(uint32_t l_word_index = 0;l_word_index < 80; ++l_word_index)
         {
             std::cout << "Word["<< l_word_index <<"] = 0x" << std::hex << m_words[l_word_index] << std::dec << std::endl ;
         }
+#endif // VERBOSE_SHA1
 
 
         // Initialising variables
@@ -227,7 +261,9 @@ sha1::sha1( const uint8_t * p_data
         //-------------------------
         for(uint32_t l_round_index = 0; l_round_index < 80 ; ++l_round_index)
         {
+#ifdef VERBOSE_SHA1
             std::cout << "Performing round " << l_round_index << std::endl ;
+#endif // VERBOSE_SHA1
             uint32_t l_temp = rotl(a,5) + f(b, c, d, l_round_index) + e + get_constant(l_round_index) + m_words[l_round_index];
             e = d;
             d = c;
@@ -243,13 +279,19 @@ sha1::sha1( const uint8_t * p_data
         m_key[3] = d + m_key[3];
         m_key[4] = e + m_key[4];
 
+#ifdef VERBOSE_SHA1
         std::cout << "Hash Value of block[" << l_block_index << "] =  ";
         display_key();
         std::cout << std::endl ;
+#endif // VERBOSE_SHA1
     }
+
+#ifdef VERBOSE_SHA1
     std::cout << "Hash Value =  ";
     display_key();
     std::cout << std::endl ;
+#endif // VERBOSE_SHA1
+
 }
 
 //------------------------------------------------------------------------------
--- src/main_sha1.cpp
+++ src/main_sha1.cpp
@@ -33,12 +33,14 @@ bool check_hash( const std::string & p_string
     bool l_ok = true;
     std::cout << R"(Check hash of ")" << p_string << R"(")" << std::endl;
     sha1 l_sha1((const uint8_t *)p_string.c_str(),8 * p_string.size());
+
     unsigned int l_key_index = 0;
     for(auto l_iter: p_reference_key)
     {
         l_ok &= quicky_utils::quicky_test::check_expected(l_iter, l_sha1.get_key(l_key_index), "Check key[" + std::to_string(l_key_index) +"]");
         ++l_key_index;
     }
+    std::cout << std::endl;
     return l_ok;
 }
 
